90cc8c28474e0342ce998d800d2e1efbfc1acefc
Alexandra-Paula <sandamanea05@gmail,com>
2025-03-20 15:01:24 +0200

    updating businesslogic with ILogin in Interfaces

---
+++ eUseControl.BusinessLogic/Interfaces/ILogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eUseControl.Domain.Model.User;
+
+namespace eUseControl.BusinessLogic.Interfaces
+{//declaram metoda
+    public interface ILogin
+    {
+        string UserAuthLogic(UserLoginDTO data);
+    }
+}
